31948ddc2489e108550a4a2f15b98bc5dd3173f3
Omkar_Chavan <83566321+omkarchavan-JH@users.noreply.github.com>
2021-12-01 13:27:27 +0530

    Delete target/classes directory

--- target/classes/db/V0.1__create_employee_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employeedetails
-(
-    account_id int,
-    employeename VARCHAR(20)
-)
-
-select * from employeedetails;
-
-insert  into employeedetails(account_id,employeename)values (1,'Ram');
